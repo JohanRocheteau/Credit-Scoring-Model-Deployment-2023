882b79068bf5a61ac3a473684127a87f9e369f61
Johan Rocheteau <johan.rocheteau@hotmail.fr>
2023-09-03 20:28:57 +0200

    Nouvelle Version

--- Flask.py
+++ Flask.py
@@ -1,28 +1,27 @@
 # Import libraries
 from flask import Flask, request
 import mlflow
+import numpy as np
 
 app = Flask(__name__)
 
-# Load the model
-model_name = "sk-learn-LGBMClassifier"
-stage = "Production"
-
-model = mlflow.sklearn.load_model(model_uri=f"models:/{model_name}/{stage}")
-
-
-@app.route('/api',methods=['POST'])
+@app.route('/api/',methods=['POST'])
 
 def predict():
-    if request.method == 'POST':
-        # Get the data from the POST request.
-        features = request.form.values()
-        
-        # Make prediction using model loaded from disk as per the data.
-        prediction = model.predict_proba(features)    
-        
-        # Take the first value of prediction    
-        return prediction
+
+    # Get the data from the POST request.
+    data = request.get_json()
+    
+    # Make prediction using model loaded from disk as per the data.
+    prediction = np.array2string(model.predict_proba(data))  
+    
+    # Take the first value of prediction    
+    return prediction
 
 if __name__ == '__main__':
+    # Load the model
+    model_name = "sk-learn-LGBMClassifier"
+    stage = "Production"
+    model = mlflow.sklearn.load_model(model_uri=f"models:/{model_name}/{stage}")
+    
     app.run(port=8080, debug=True)
