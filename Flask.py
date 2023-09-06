0196046a103c007224dc3db29c129e7a8d25c842
JohanRocheteau <89990380+JohanRocheteau@users.noreply.github.com>
2023-09-06 23:02:28 +0200

    Add files via upload

--- Flask.py
+++ Flask.py
@@ -13,15 +13,15 @@ def predict():
     data = request.get_json()
     
     # Make prediction using model loaded from disk as per the data.
-    prediction = np.array2string(model.predict_proba(data))  
+    prediction = np.array2string(model.predict_proba(data)) 
+    
     
     # Take the first value of prediction    
     return prediction
 
 if __name__ == '__main__':
     # Load the model
-    model_name = "sk-learn-LGBMClassifier"
-    stage = "Production"
-    model = mlflow.sklearn.load_model(model_uri=f"models:/{model_name}/{stage}")
+    model_uri = "mlflow-artifacts:/8cef877a90864d9ea9a1432b58f8e5d3/dd8c1ebb7e1b4929bcbc968736cf8e0d/artifacts/sklearn-model"
+    model = mlflow.sklearn.load_model(model_uri)
     
     app.run(port=8080, debug=True)
