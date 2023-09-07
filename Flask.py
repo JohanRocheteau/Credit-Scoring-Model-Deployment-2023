8e3304ac2fda382b3bf675724d3cfb148b778772
JohanRocheteau <89990380+JohanRocheteau@users.noreply.github.com>
2023-09-07 10:33:32 +0200

    Add files via upload

--- Flask.py
+++ Flask.py
@@ -3,6 +3,7 @@ from flask import Flask, request
 import mlflow
 import numpy as np
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -16,7 +17,6 @@ def predict():
     # Make prediction using model loaded from disk as per the data.
     prediction = np.array2string(model.predict_proba(data)) 
     
-    
     # Take the first value of prediction    
     return prediction
 
@@ -25,4 +25,4 @@ if __name__ == '__main__':
     model = pickle.load(open('ModelGrid.sav', 'rb'))
     #model = mlflow.sklearn.load_model(model_uri)
     
-    #app.run(port=8080, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
