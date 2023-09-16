ea30210ff872c48bcaff7b8b5da9b4b9c08e0688
Johan Rocheteau <johan.rocheteau@hotmail.fr>
2023-09-16 18:22:04 +0200

    Tests modifications

--- Applications/AppFlask.py
+++ Applications/AppFlask.py
@@ -8,9 +8,9 @@ app = Flask(__name__)
 
 # Load the model :
 try :
-    model = pickle.load(open(r'C:\Users\Johan\Formation Data Science\Projet 7\ProjetDSN7\Applications\ModelGrid.sav', 'rb'))
+    model = pickle.load(open(r'C:\Users\Johan\Formation Data Science\Projet 7\ProjetDSN7\Applications\Donneesgenerees\ModelGrid.sav', 'rb'))
 except :
-    model = pickle.load(open(r'ModelGrid.sav', 'rb'))
+    model = pickle.load(open('Applications/Donneesgenerees/ModelGrid.sav', 'rb'))
 
 @app.route('/api/',methods=['POST'])
 
